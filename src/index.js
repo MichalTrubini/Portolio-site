c22838e968231abf94b433205bec00ac8a9ae2a4
Michal Trubini <mchl.trubini@gmail.com>
2023-03-10 10:46:57 +0100

    GA

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import ReactGA from "react-ga4";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ReactGA.initialize("G-CBTLXBG0KY");
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
